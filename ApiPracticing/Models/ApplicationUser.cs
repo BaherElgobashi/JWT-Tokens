b7082ab59897535e7f832cc452a48be5efe29731
BaherElgobashi <bahermohamedelgobashi@gmail.com>
2025-11-11 20:48:42 +0200

    Add RefreshTokenModel and Make relation with Application User Model

--- ApiPracticing/Models/ApplicationUser.cs
+++ ApiPracticing/Models/ApplicationUser.cs
@@ -9,5 +9,7 @@ namespace ApiPracticing.Models
         public string FirstName { get; set; }
         [Required, MaxLength(50)]
         public string LastName { get; set; }
+
+        public List<RefreshToken> RefreshTokens { get; set; }
     }
 }
